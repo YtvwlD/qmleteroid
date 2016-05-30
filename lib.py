a91469b525849ac94e74328e0616846de5dfe6da
Niklas Sombert <niklas@ytvwld.de>
2016-05-30 10:29:05 +0200

    It's `data`, not `content`.
    This should fix most of https://landscape.io/github/YtvwlD/qmleteroid/1/messages/error.

--- lib.py
+++ lib.py
@@ -1,16 +1,16 @@
 # QMLeteroid - a QML clone of https://github.com/chaosdorf/meteroid
 # Copyright (C) 2016 Niklas Sombert
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -52,11 +52,11 @@ def add_user(name, balance, email):
 
 def _parseUser(data):
 	return {
-		"id": content["id"],
-		"name": content["name"],
-		"balance": content["balance"],
-		"email": content["email"],
-		"portrait": "http://gravatar.com/avatar/{}".format(md5(content["email"].strip().lower().encode()).hexdigest())
+		"id": data["id"],
+		"name": data["name"],
+		"balance": data["balance"],
+		"email": data["email"],
+		"portrait": "http://gravatar.com/avatar/{}".format(md5(data["email"].strip().lower().encode()).hexdigest())
 	}
 
 def get_drinks():
